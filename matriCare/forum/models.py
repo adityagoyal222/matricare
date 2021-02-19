c87ed3b766b9c084514f4c4ddc7d647682cfc450
Aditya Goyal <goyaladitya85@gmail.com>
2021-02-19 15:50:01 +0545

    changed models.py in forum

--- matriCare/forum/models.py
+++ matriCare/forum/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    text_content = models.CharField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="author")
+    likes = models.PositiveIntegerField(default=0)
+    created_date = models.DateTimeField(auto_now_add=True)
+    
+class PostImage(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="images")
+    image = models.ImageField(upload_to='post_images')
+
