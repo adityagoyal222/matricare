562aa84fc3710226c3b4231d419d8617ef43f393
Aditya Goyal <goyaladitya85@gmail.com>
2021-02-19 16:06:06 +0545

    created comment model and added meta

--- matriCare/forum/models.py
+++ matriCare/forum/models.py
@@ -2,12 +2,24 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    text_content = models.CharField()
+    text_content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="author")
     likes = models.PositiveIntegerField(default=0)
     created_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_date']
     
 class PostImage(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="images")
     image = models.ImageField(upload_to='post_images')
 
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="author")
+    comment = models.CharField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    likes = models.PositiveIntegerField(default=0)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comment")
+
+    class Meta:
+        ordering = ['-created_date']
